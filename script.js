6802d07f65d15fa21bed19b8da27762b6d874fa8
soju <frankyiyi@gmail.com>
2018-04-14 20:30:56 -0700

    updated index.html with about me description and fixed spacing between sections

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ window.addEventListener('scroll', fixNav);
 particlesJS.load('particles-js', 'particlesjs-config.json', () => {
   console.log('particles.json loaded...')
 })
+
+const model = {
+  aboutInfo: "Hello, my name is Frank Yi and I am a Javascript developer from Tacoma Washington.  \
+  I love computers, coding, and all things technological.  I use HTML, CSS, and code mainly in Vanilla Javascript.  \
+  I like to create projects about Seattle and based around Seattle's open data API. Here are a few examples below."
+}
+
+document.querySelector('.about-text-p').innerHTML = model.aboutInfo;
